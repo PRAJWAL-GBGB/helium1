187f2819f9a1c7ba372a5becbef52208ee49d93d
Prajwal <prajju0002@gmail.com>
2025-03-11 15:17:06 +0530

    refactor: simplify server setup and remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(app);
