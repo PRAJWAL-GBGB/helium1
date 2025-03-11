9a1988b76d9574cc706b5497fc74d443aac7aae2
Prajwal <prajju0002@gmail.com>
2025-03-11 15:13:28 +0530

    initialized

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { handle } from 'hono/aws-lambda'
 
 const app = new Hono()
 
@@ -7,4 +7,9 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-export const handler = handle(app)
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
